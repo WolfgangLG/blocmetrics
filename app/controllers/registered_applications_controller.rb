232a6bc86e9379a757c5b5e2774c70ed9b1b35dd
Ludwig Gerdes <lgerdes@g2crowd.com>
2017-04-30 14:49:33 -0500

    Create registered applications controller with index action and customized views

---
+++ app/controllers/registered_applications_controller.rb
@@ -0,0 +1,49 @@
+class RegisteredApplicationsController < ApplicationController
+  before_action :set_registered_application, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @registered_applications = current_user.registered_applications.all
+  end
+
+  # def show
+  # end
+  #
+  # def new
+  #   @registered_application = RegisteredApplication.new
+  # end
+  #
+  # def edit
+  # end
+  #
+  # def create
+  #   @registered_application = RegisteredApplication.new(registered_application_params)
+  #
+  #   if @registered_application.save
+  #     redirect_to @registered_application, notice: 'Registered application was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
+  #
+  # def update
+  #   if @registered_application.update(registered_application_params)
+  #     redirect_to @registered_application, notice: 'Registered application was successfully updated.'
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #
+  # def destroy
+  #   @registered_application.destroy
+  #   redirect_to registered_applications_url, notice: 'Registered application was successfully destroyed.'
+  # end
+
+  private
+    def set_registered_application
+      @registered_application = RegisteredApplication.find(params[:id])
+    end
+
+    def registered_application_params
+      params.require(:registered_application).permit(:name, :url, :user_id)
+    end
+end
