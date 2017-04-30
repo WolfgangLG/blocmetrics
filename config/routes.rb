14d7d024a752eec80361d608dcca2d8d33473c60
Ludwig Gerdes <lgerdes@g2crowd.com>
2017-04-30 10:32:44 -0500

    Give users a name and create user factory and seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   root 'welcome#index'
 end
