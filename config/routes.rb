0a75f80de3958c5d966833a51f4491416b747234
Ludwig Gerdes <lgerdes@g2crowd.com>
2017-05-03 23:50:24 -0500

    Create events controller and routes with stubbed tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   root 'welcome#index'
   resources :registered_applications
+
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
 end
